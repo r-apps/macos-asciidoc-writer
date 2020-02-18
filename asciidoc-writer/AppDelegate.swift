e8ebdc15d71939e7220df66222408c4dd1fc12ae
Ruben Vitt <r.vitt@fme.de>
2020-02-18 14:39:53 +0100

    Initial Commit

---
+++ asciidoc-writer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  asciidoc-writer
+//
+//  Created by Ruben Vitt on 18.02.20.
+//  Copyright © 2020 Rubeen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ asciidoc-writer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  asciidoc-writer
+//
+//  Created by Ruben Vitt on 18.02.20.
+//  Copyright © 2020 Rubeen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
